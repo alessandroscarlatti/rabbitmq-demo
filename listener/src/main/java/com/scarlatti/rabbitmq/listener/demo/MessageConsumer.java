f724e79649efc9fbfe06ef6e7c923c4bf9fb0ebd
alessandroscarlatti <violanotesnoextras@gmail.com>
2018-01-22 18:11:19 -0600

    initial commit with rabbit listener demo

---
+++ listener/src/main/java/com/scarlatti/rabbitmq/listener/demo/MessageConsumer.java
@@ -0,0 +1,24 @@
+package com.scarlatti.rabbitmq.listener.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * ______    __                         __           ____             __     __  __  _
+ * ___/ _ | / /__ ___ ___ ___ ____  ___/ /______    / __/______ _____/ /__ _/ /_/ /_(_)
+ * __/ __ |/ / -_|_-<(_-</ _ `/ _ \/ _  / __/ _ \  _\ \/ __/ _ `/ __/ / _ `/ __/ __/ /
+ * /_/ |_/_/\__/___/___/\_,_/_//_/\_,_/_/  \___/ /___/\__/\_,_/_/ /_/\_,_/\__/\__/_/
+ * Monday, 1/22/2018
+ */
+@Component
+public class MessageConsumer {
+
+    private final static Logger log = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @RabbitListener(queues = "${rabbitmq.consumer.queue}")
+    void receiveMessage(byte[] bytes) {
+        log.info("received message: " + new String(bytes));
+    }
+}
---
+++ listener/src/main/java/com/scarlatti/rabbitmq/listener/demo/RabbitConfig.java
@@ -0,0 +1,30 @@
+package com.scarlatti.rabbitmq.listener.demo;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ______    __                         __           ____             __     __  __  _
+ * ___/ _ | / /__ ___ ___ ___ ____  ___/ /______    / __/______ _____/ /__ _/ /_/ /_(_)
+ * __/ __ |/ / -_|_-<(_-</ _ `/ _ \/ _  / __/ _ \  _\ \/ __/ _ `/ __/ / _ `/ __/ __/ /
+ * /_/ |_/_/\__/___/___/\_,_/_//_/\_,_/_/  \___/ /___/\__/\_,_/_/ /_/\_,_/\__/\__/_/
+ * Monday, 1/22/2018
+ */
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    @ConfigurationProperties("rabbitmq.connection")
+    ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory();
+    }
+
+    @Bean
+    SimpleMessageListenerContainer listenerContainer(ConnectionFactory connectionFactory) {
+        return new SimpleMessageListenerContainer(connectionFactory);
+    }
+}
